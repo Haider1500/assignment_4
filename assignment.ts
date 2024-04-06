3c1dbbf7012abcc72a20d817ddc00457399cac15
Ali Haider <ali1500haider@gmail.com>
2024-04-06 10:07:02 +0500

    till first 4 questions

---
+++ assignment.ts
@@ -0,0 +1,26 @@
+// 1. Hello Variable
+let greeting = "Hello, World!";
+console.log(greeting, "=======ans1");
+
+// 2. Basic Math
+let num1 = 20;
+let num2 = 10;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+console.log(sum, difference, product, quotient);
+
+// 3. Swapping Values
+//   b = 2; // Swap these values without using a new variable.
+let a = 1,
+  b = 2;
+
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, "====a", b, "======b`");
+
+// 4. Type Annotation (TypeScript)
+let message: string;
+// message = 15;
