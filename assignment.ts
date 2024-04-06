75c0b1c21f6deb5cadcd45c77dee308a3257c2f7
Ali Haider <ali1500haider@gmail.com>
2024-04-06 12:52:26 +0500

    done till last question

--- assignment.ts
+++ assignment.ts
@@ -2,6 +2,8 @@
 let greeting = "Hello, World!";
 console.log(greeting, "=======ans1");
 
+// ---------------------------------------------------------------------------
+
 // 2. Basic Math
 let num1 = 20;
 let num2 = 10;
@@ -11,15 +13,19 @@ let product = num1 * num2;
 let quotient = num1 / num2;
 console.log(sum, difference, product, quotient);
 
+// --------------------------s-------------------------------------------------
+
 // 3. Swapping Values
 //   b = 2; // Swap these values without using a new variable.
-// let a = 1,
-//   b = 2;
+let a = 1,
+  b = 2;
 
-// a = a + b;
-// b = a - b;
-// a = a - b;
-// console.log(a, "====a", b, "======b`");
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a, "====a", b, "======b`");
+
+// ---------------------------------------------------------------------------
 
 // 4. Type Annotation (TypeScript)
 let message: string;
@@ -32,22 +38,28 @@ let number1 = 11,
 let remainder = number1 % number2;
 console.log(remainder);
 
+// ---------------------------------------------------------------------------
+
 // 6. Increment Challenge
 // Instructions: Increment a variable's value by 1 using two different methods.
 let counter = 1;
 let increasedValue1 = counter++;
 let increasedValue2 = counter + 1;
 
-// 6.  Logical gates
-let a = true,
-  b = false,
-  c = true; // Write expressions using these.
+// ---------------------------------------------------------------------------
 
-let and = a && b && c;
-let or = a || b || c;
-let notA = !a;
-let notB = !b;
-let notC = !c;
+// 7.  Logical gates
+let u = true,
+  v = false,
+  w = true; // Write expressions using these.
+
+let and = u && v && w;
+let or = u || v || w;
+let notA = !u;
+let notB = !v;
+let notC = !w;
+
+// ---------------------------------------------------------------------------
 
 // 8. Compound Assignment
 let num = 10; // Use +=, -=, *=, and /= on this variable.
@@ -56,14 +68,18 @@ let decrement = (num -= 2); //8
 let multiplication = (num *= 2); //20
 let division = (num /= 2); //5
 
+// ---------------------------------------------------------------------------
+
 // 9. Even or Odd
 // Instructions: Write a program to check if a number is even or odd.
 
 let value: number = 20;
-let isEven = value % 2 == 0;
+let isEven = value % 2 == 0; //using the modulus we get the remainder and for even remainder is zero
 if (isEven) {
   console.log(`Number ${value} is even`);
 } else console.log(`Number ${value} is odd`);
+
+// ---------------------------------------------------------------------------
 
 //10. Voting Eligibility
 // Instructions: Check if a person is eligible to vote.
@@ -73,6 +89,8 @@ let isEighteen = age >= 18;
 if (isEighteen) {
   console.log("You are eligible to vote!");
 } else console.log("Sorry! You are under age.");
+
+// ---------------------------------------------------------------------------
 
 // 11. Grading System
 // Instructions: Assign a grade based on a numerical score.
@@ -92,6 +110,8 @@ if (score >= 80) {
 }
 console.log(`Your grade is ${grade}`);
 
+// ---------------------------------------------------------------------------
+
 // 12. Max of Three
 // Instructions: Find the maximum of three numbers.
 
@@ -107,5 +127,49 @@ if (x > y && x > z) {
 }
 
 // Optimized solution
-let largest = Math.max(x, y, z);
+let largest = Math.max(x, y, z); //using inbuilt Js Math function to find the largest number
 console.log(`${largest} is the largest number`);
+
+// ---------------------------------------------------------------------------
+
+// 13. Leap Year Checker
+// Instructions: Check if a given year is a leap year.
+
+let year: number = 1700;
+// if year is completely divisible by 400 its leap year i.e 1200, 1600, 2000 are leap yrs
+// Or if the year is divisble by 4 and is not divisble by 100 then its a leap year
+let isLeapYear = year % 400 == 0 || (year % 4 === 0 && year % 100 != 0);
+
+if (isLeapYear) {
+  console.log(`${year} is a Leap year!`);
+} else console.log(`${year} is not a leap year!`);
+
+// ---------------------------------------------------------------------------
+
+// 14. Fahrenheit to Celsius Converter
+// Instructions: Write a program that converts temperature from Fahrenheit to Celsius.
+
+let fahrenheit = 96; // Convert this to Celsius and print the result.
+
+// formula to convert is cels = (farenheit - 32.0) * 5.0/9.0
+let celcius = ((fahrenheit - 32) * 5) / 9;
+console.log(Math.round(celcius), "=========celcius");
+
+// 15. Positive, Negative, or Zero
+// Instructions: Check if a number is positive, negative, or zero.
+
+let number = 0; // Determine the sign of this number.
+let sign;
+if (number > 0) {
+  sign = "positive";
+} else if (number < 0) {
+  sign = "negative";
+} else sign = "zero";
+console.log(`Number is ${sign}`);
+
+// 16. Multiplication Table
+// Instructions: Write a program that prints the multiplication table of a given number up to 10.
+let givenNumber = 2; // Print the multiplication table for this number up to 10.
+for (let i = 1; i <= 10; i++) {
+  console.log(`${givenNumber} x ${i} = ${givenNumber * i}`);
+}
